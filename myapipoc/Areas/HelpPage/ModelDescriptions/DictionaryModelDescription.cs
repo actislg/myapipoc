f79afdad81f0c62ea8f1936cc4cbf85f4a601a93
Gabriel Actis Aversa <gactisaversa1@interlinebrands.com>
2017-11-01 14:25:47 -0400

    Add project files.

---
+++ myapipoc/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace myapipoc.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ myapipoc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="myapipoc.WebApiApplication" Language="C#" %>
